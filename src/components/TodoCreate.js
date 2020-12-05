e31c5ef6ef9d671d8d1c900eaef5f480bd470c0b
joyAct <joyleeact@gmail.com>
2020-12-05 21:54:19 +0900

    프로젝트 생성 UI 준비 - 항목 추가 input 창 생성

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -46,14 +46,49 @@ const CircleButton = styled.button`
     `}
 `;
 
+const InserttFormPositioner = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+`;
+
+const InsertForm = styled.div`
+  background: #f8f9fa;
+  padding: 32px;
+  padding-bottom: 72px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #e9ecef;
+`;
+
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+`;
+
 function TodoCreate() {
   const [open, setOpen] = useState(false);
   const handleToggle = () => setOpen(!open);
 
   return (
-    <CircleButton onClick={handleToggle} open={open}>
-      <MdAdd />
-    </CircleButton>
+    <>
+      {open && (
+        <InserttFormPositioner>
+          <InsertForm>
+            <Input placeholder="할 일을 입력 후, Enter를 누르세요" autoFocus />
+          </InsertForm>
+        </InserttFormPositioner>
+      )}
+      <CircleButton onClick={handleToggle} open={open}>
+        <MdAdd />
+      </CircleButton>
+    </>
   );
 }
 
